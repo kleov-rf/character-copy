4b365c8db1ff0ab1ea8c33be6d6778d7873c2a31
K Leonardo Ruiz Flórez <kleonardoruizflorez@gmail.com>
2023-05-30 12:31:10 +0200

    Change destination from mock to spy

--- src/test/java/org/characterCopy/CopierShould.java
+++ src/test/java/org/characterCopy/CopierShould.java
@@ -1,6 +1,7 @@
 package org.characterCopy;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Spy;
 
 import static org.mockito.Mockito.*;
 
@@ -8,7 +9,7 @@ public class CopierShould {
 
     @Test
      void read_characters_from_source_and_copy_to_destination() {
-        var destination = mock(Destination.class);
+        var destination = spy(Destination.class);
         var source = mock(Source.class);
 
         when(source.getChar()).thenReturn('\n');
