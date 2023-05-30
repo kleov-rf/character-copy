5179e2c8007aa65d5907c1a4a29553cc851a311f
K Leonardo Ruiz Flórez <kleonardoruizflorez@gmail.com>
2023-05-30 12:19:05 +0200

    Fixed failing test

--- src/main/java/org/characterCopy/Copier.java
+++ src/main/java/org/characterCopy/Copier.java
@@ -1,10 +1,17 @@
 package org.characterCopy;
 
 public class Copier {
+    private final SourceStub source;
+    private final DestinationSpy destination;
+
     public Copier(SourceStub source, DestinationSpy destination) {
-        
+
+        this.source = source;
+        this.destination = destination;
     }
 
     public void copy() {
+        char characterToCopy = source.getChar();
+        destination.setChar(characterToCopy);
     }
 }
--- src/main/java/org/characterCopy/SourceStub.java
+++ src/main/java/org/characterCopy/SourceStub.java
@@ -3,6 +3,6 @@ package org.characterCopy;
 public class SourceStub implements Source {
     @Override
     public char getChar() {
-        throw new UnsupportedOperationException("Don't forget to implement me!");
+        return '\n';
     }
 }
