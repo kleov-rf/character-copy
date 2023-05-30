9077583d0732513695f2720de3196e9832165af5
K Leonardo Ruiz Flórez <kleonardoruizflorez@gmail.com>
2023-05-30 12:39:08 +0200

    Moved destination spy and source mock to fields of CopierShould

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,8 @@ repositories {
 
 dependencies {
     implementation("org.mockito:mockito-core:5.2.0")
+    implementation("org.mockito:mockito-junit-jupiter:5.2.0")
+    implementation("org.mockito:mockito-junit-jupiter:5.2.0")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
--- src/test/java/org/characterCopy/CopierShould.java
+++ src/test/java/org/characterCopy/CopierShould.java
@@ -1,17 +1,22 @@
 package org.characterCopy;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 public class CopierShould {
+
+    @Spy public Destination destination;
+    @Mock
+    public Source source;
 
     @Test
      void read_characters_from_source_and_copy_to_destination() {
-        var destination = spy(Destination.class);
-        var source = mock(Source.class);
-
         when(source.getChar()).thenReturn('\n');
 
         Copier copier = new Copier(source, destination);
